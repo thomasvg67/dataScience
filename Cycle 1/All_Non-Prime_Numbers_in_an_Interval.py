c55a0279cd558daaa1037099cf3ee9d3ebcb8b88
thomasvg67 <thomasvgvpkl123@gmail.com>
2024-07-29 18:04:46 +0530

    Cycle 1

---
+++ Cycle 1/All_Non-Prime_Numbers_in_an_Interval.py
@@ -0,0 +1,14 @@
+a = int(input("Enter starting range: "))
+b = int(input("Enter ending range: "))
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, (num // 2) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+for num in range(a, b + 1):
+    if not is_prime(num):
+        print(num)
